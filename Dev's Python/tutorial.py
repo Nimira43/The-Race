6fa72809069295d800cba02df82faedcce92d014
Nick Rabone <60065507+Nimira43@users.noreply.github.com>
2023-02-18 21:28:38 +0000

    Add files via upload
    First batch

---
+++ Dev's Python/tutorial.py
@@ -0,0 +1,38 @@
+y = 5 * (7-4*3) - (6+8/5) * 4**3
+x = 3
+z = y*x
+print(y)
+print(z)
+b="6"
+print(type(b))
+a="lenny"
+print(a)
+print(a.upper())
+u=47
+t=70
+print(u/t*100)
+num1 = int
+num2 = int
+#input num1
+#input num2
+#num3=num1/num2*100
+#print(num3)
+t="manchesterunitedfootballclub"
+print(t[5])
+print(t[10:16])
+print('hello ' + a + ', do you support ' + t[10:16])
+drivers = ["senna","piquet","mansell","prost"]
+wc = [3, 3, 1, 4]
+years = [81, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93]
+#print(drivers[1] + wc[0]) + years[0, 1, 4])
+#print(drivers[0], wc[1], years[5, 7, 8])
+#print(drivers[2], wc[2], years[9])
+#print(drivers[3], wc[3], years[2, 3, 6, 10])
+#drivers.append["schumacher"]
+#print(drivers)
+k = 465445645744765 / 6767676767676 
+print (k)
+d = input("enter the mark - ")
+e = input("enter max marks - ")
+f = d/e*100
+print("percentage - ", f)
