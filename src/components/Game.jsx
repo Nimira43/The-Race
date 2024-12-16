25b213754ade80716e46b5785d4366250a210d77
Nimira43 <n.rabone72@aol.com>
2024-12-16 03:54:23 +0000

    Setting up Player

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Car from './Car'
 import RivalCar from './RivalCar'
 import '../styles/Game.css'
 
 const Game = () => {
+  const [player, setPlayer] = useState({ 
+    x: 0, 
+    y: 0, 
+    speed: 5, 
+    score: 0
+  })
+
   return (
     <div className='game-container'>
       <h1>Game</h1>
+      <div className="score">Score: 50</div>
       <Car/>
       <RivalCar />
     </div>
